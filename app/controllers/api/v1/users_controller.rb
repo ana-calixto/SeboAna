495a47ee7b376e7308d314c2200c5ff143bb150f
ana calixto <aninhacalixto@gmail.com>
2023-11-21 21:20:06 -0300

    user controler with serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -20,7 +20,8 @@ class Api::V1::UsersController < ApplicationController
   def create 
     @user = User.new(user_params) 
     if @user.save 
-      render json: @user, status: :created
+      render json: UserSerializer.new(@user).serializable_hash.to_json, status: :created
+      # render json: @user, status: :created
     else 
       render json: @user.errors, status: :unprocessable_entity
     end 
